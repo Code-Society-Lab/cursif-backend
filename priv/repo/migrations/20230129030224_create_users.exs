b8d729b80ee2a64571f07e0350245c6adddc5ab7
Simon Roy <simon.roy1211@gmail.com>
2023-01-28 22:23:34 -0500

    Removed users_token table

--- priv/repo/migrations/20230129030224_create_users.exs
+++ priv/repo/migrations/20230129030224_create_users.exs
@@ -16,16 +16,5 @@ defmodule Cursif.Repo.Migrations.CreateUsers do
 
     create unique_index(:users, [:email])
     create unique_index(:users, [:username])
-
-    create table(:users_tokens) do
-      add :user_id, references(:users, on_delete: :delete_all), null: false
-      add :token, :binary, null: false
-      add :context, :string, null: false
-      add :sent_to, :string
-      timestamps(updated_at: false)
-    end
-
-    create index(:users_tokens, [:user_id])
-    create unique_index(:users_tokens, [:context, :token])
   end
 end
