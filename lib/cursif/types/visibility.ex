7ac6ab4e73217ac1a07a2ae311b081a65d43876e
Simon Roy <simon.roy1211@gmail.com>
2023-03-29 03:32:58 -0400

    Fix vibility type loading

--- lib/cursif/types/visibility.ex
+++ lib/cursif/types/visibility.ex
@@ -11,15 +11,9 @@ defmodule Cursif.Visibility do
   def type, do: :string
 
   for visibility <- visibilities do
-    def cast(unquote(visibility)), do: {:ok, unquote(visibility)}
-    def cast(unquote(to_string(visibility))), do: {:ok, unquote(visibility)}
-
-    def load(unquote(visibility)), do: {:ok, unquote(visibility)}
-
-    def dump(unquote(visibility)), do: {:ok, unquote(visibility)}
-    def dump(unquote(to_string(visibility))), do: {:ok, unquote(visibility)}
-
-    def cast(_other), do: :error
-    def dump(_other), do: :error
+    def cast(visibility), do: {:ok, visibility}
+    def cast(_), do: :error
+    def load(visibility), do: {:ok, visibility}
+    def dump(visibility), do: {:ok, visibility}
   end
 end
