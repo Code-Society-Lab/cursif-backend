7df4e7eb9755c72c0cbda5c270fb144d6b929806
Chris <rolletchris1@gmail.com>
2023-07-30 09:08:32 -0400

    UpdateOrganization by ID

--- lib/cursif_web/resolvers/organizations.ex
+++ lib/cursif_web/resolvers/organizations.ex
@@ -16,6 +16,14 @@ defmodule CursifWeb.Resolvers.Organizations do
       Phoenix.log(error)
       {:error, :not_found}
     end
+
+    @spec get_by_id(map(), map()) :: {:ok, Organization.t()}
+    def get_by_id(%{id: id}, _context) do
+      {:ok, Organizations.get_by_id!(id)}
+    rescue error ->
+      Phoenix.log(error)
+      {:error, :not_found}
+    end
   
     @spec create_organization(map(), map()) :: {:ok, Organization.t()}
     def create_organization(organization, _context) do
@@ -26,8 +34,8 @@ defmodule CursifWeb.Resolvers.Organizations do
     end
 
     @spec update_organization(map(), map()) :: {:ok, Organization.t()} | {:error, atom()}
-    def update_organization(%{name: name} = args, _context) do
-      organization = Organizations.get_by_name(name)
+    def update_organization(%{id: id} = args, _context) do
+      organization = Organizations.get_by_id!(id)
 
       case Organizations.update_organization(organization, args) do
         {:ok, organization} -> {:ok, organization}
