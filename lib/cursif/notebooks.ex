8e2ff8f81f29fadd73e140756e33a60ba85ab374
Simon Roy <simon.roy1211@gmail.com>
2023-07-30 01:21:26 -0400

    Remove children association loading of notebook pages

--- lib/cursif/notebooks.ex
+++ lib/cursif/notebooks.ex
@@ -35,7 +35,7 @@ defmodule Cursif.Notebooks do
       ** (Ecto.NoResultsError)
 
   """
-  def get_notebook!(id), do: Repo.get!(Notebook, id) |> Repo.preload(pages: [:children, :author])
+  def get_notebook!(id), do: Repo.get!(Notebook, id) |> Repo.preload(pages: [:author])
 
   @doc """
   Creates a notebook.
