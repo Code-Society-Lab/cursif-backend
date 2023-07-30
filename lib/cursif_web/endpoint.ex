e341c4e7af0732c42fa3f52cc210806a77b21b51
MaanasArora <maanasarora23@gmail.com>
2023-07-29 21:49:02 -0400

    Add CORS plug

--- lib/cursif_web/endpoint.ex
+++ lib/cursif_web/endpoint.ex
@@ -41,6 +41,8 @@ defmodule CursifWeb.Endpoint do
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
 
+  plug CORSPlug
+
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
