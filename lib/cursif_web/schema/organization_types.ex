6d1038ea9afdf5e85ae13b9b696fe8807bdd0607
Chris Dedman-Rollet <61106361+chrisdedman@users.noreply.github.com>
2023-07-29 23:29:18 -0400

    added update organization mutation

--- lib/cursif_web/schema/organization_types.ex
+++ lib/cursif_web/schema/organization_types.ex
@@ -46,5 +46,13 @@ defmodule CursifWeb.Schema.OrganizationTypes do
 
         resolve(&Organizations.create_organization/2)
         end
+
+        @desc "Update an organization"
+        field :update_organization, :organization do
+        arg(:name, :string)
+        arg(:owner_id, non_null(:id))
+
+        resolve(&Organizations.update_organization/2)
+        end
     end
 end
