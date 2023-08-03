cd6d26b00921ba4fcf014c85afc1fc90af8c3098
Chris <rolletchris1@gmail.com>
2023-08-03 18:41:02 -0400

    Update a user

--- lib/cursif_web/resolvers/accounts.ex
+++ lib/cursif_web/resolvers/accounts.ex
@@ -33,11 +33,12 @@ defmodule CursifWeb.Resolvers.Accounts do
 
   @spec update_user(map(), map()) :: {:ok, User.t()} | {:error, atom()}
   def update_user(%{id: id} = args, _context) do
-    user = Accounts.get_user!(id)
-    case Accounts.update_user(user, args) do
-        {:ok, user} -> {:ok, user}
-        {:error, changeset} -> {:error, changeset}
-    end
+      user = Accounts.get_user!(id)
+
+      case Accounts.update_user(user, args) do
+          {:ok, user} -> {:ok, user}
+          {:error, changeset} -> {:error, changeset}
+      end
   end
 
   @spec login(%{email: String.t(), password: String.t()}, map()) :: {:ok, User.t()} | {:error, list(map())}
