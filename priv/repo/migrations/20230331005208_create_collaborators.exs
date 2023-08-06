e2faf7a0877fcb6185431e5b041466e70753d2a5
Chris <rolletchris1@gmail.com>
2023-08-06 08:38:58 -0400

    update collab table

--- priv/repo/migrations/20230331005208_create_collaborators.exs
+++ priv/repo/migrations/20230331005208_create_collaborators.exs
@@ -2,7 +2,8 @@ defmodule Cursif.Repo.Migrations.CreateCollaborators do
   use Ecto.Migration
 
   def change do
-    create table(:collaborators) do
+    create table(:collaborators, primary_key: false) do
+      add :id, :binary_id, primary_key: true
       add :notebook_id, references(:notebooks, type: :binary_id, on_delete: :delete_all)
       add :user_id, references(:users, type: :binary_id, on_delete: :delete_all)
 
