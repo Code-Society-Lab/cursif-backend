855085661c0d410ffc885b060fa538a7b47d78c6
Chris <rolletchris1@gmail.com>
2023-09-01 18:03:55 -0400

    Fix Plug with Halt. trying to change to middleware

--- lib/cursif_web/plugs/rate_limit.ex
+++ lib/cursif_web/plugs/rate_limit.ex
@@ -3,32 +3,32 @@ defmodule CursifWeb.RateLimit do
   This module is a plug that will rate limit requests based on the client's IP address.
   """
   @behaviour Plug
+  import Plug.Conn
   alias Hammer
 
+  @impl true
   def init(options), do: options
 
+  @impl true
   def call(conn, _opts) do
     rate_limit(conn)
   end
 
   defp rate_limit(conn) do
-    if client_exceeds_limit?(conn) do
-      conn
-      |> Plug.Conn.put_status(:too_many_requests)
-      |> Plug.Conn.put_resp_header("retry-after", "60")
-      |> Plug.Conn.send_resp(:too_many_requests, "Rate limit exceeded")
-    else
-      conn
-    end
-  end
-
-  # Check if the client has exceeded the rate limit
-  defp client_exceeds_limit?(conn) do
     client_ip = get_client_ip(conn)
-    # Check if the user has exceeded the rate limit based on their IP address
     case Hammer.check_rate(client_ip, 60_000, 60) do
-      {:allow, _count} -> false
-      {:deny, _limit} -> true
+      {:allow, _count} -> conn
+      {:deny, _limit} -> 
+        conn 
+        |> put_resp_header("retry-after", "60") 
+        |> send_resp(
+          429,
+          Jason.encode!(%{
+            details: "Too many requests.",
+            status: 429
+          })
+        )
+        |> halt()
     end
   end
 
