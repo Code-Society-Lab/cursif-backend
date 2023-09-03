69206711228e04888666404d1112ce799d51c841
Chris <rolletchris1@gmail.com>
2023-09-02 23:35:36 -0700

    update email

--- lib/cursif/accounts/user_notifier.ex
+++ lib/cursif/accounts/user_notifier.ex
@@ -1,11 +1,11 @@
 defmodule Cursif.Accounts.UserNotifier do
   import Bamboo.Email
   alias Cursif.Mailer
-  
+
   def welcome_email(user) do
     email =
     new_email()
-    |> from("christopher_dedm3957@elcamino.edu")
+    |> from("cursif@example.edu")
     |> to(user.email)
     |> subject("Welcome to Cursif!")
     |> text_body("Welcome, #{user.username}!")
