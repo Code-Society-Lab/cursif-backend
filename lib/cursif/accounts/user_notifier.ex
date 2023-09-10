42096204119cd274f5084d5892c900185556c7e5
Chris <rolletchris1@gmail.com>
2023-09-10 08:32:38 -0700

    Update welcome message and refactor

--- lib/cursif/accounts/user_notifier.ex
+++ lib/cursif/accounts/user_notifier.ex
@@ -2,16 +2,38 @@ defmodule Cursif.Accounts.UserNotifier do
   import Swoosh.Email
   alias Cursif.Mailer
 
-  def welcome_email() do
+  @doc """
+  Deliver a welcome email.
+  """
+  defp welcome_email(recipient, subject, body) do
     email =
-    new()
-    |> from("test@gmail.com")
-    |> to("test@gmail.com")
-    |> subject("Welcome to Cursif!")
-    |> text_body("Welcome!")
+      new()
+      |> from({"Cursif", "cursif@codesociety.xyz"})
+      |> to(recipient)
+      |> subject(subject)
+      |> text_body(body)
     
     with {:ok, _metadata} <- Mailer.deliver(email) do
       {:ok, email}
     end
+  end
+
+   @doc """
+  Deliver instructions to confirm account.
+  """
+  def deliver_confirmation_instructions(user, url) do
+    welcome_email(user.email, "Welcome to Cursif ~ Email Verification", """
+    ------------------------------
+    Hi #{user.username},
+
+    Welcome to Cursif!
+    
+    You can confirm your account by visiting the URL below:
+
+    #{url}
+
+    If you did not create an account with us, please ignore this.
+    ------------------------------
+    """)
   end
 end
