842bc65914ad243b2f46c7210642f3844a1ebab3
Chris <rolletchris1@gmail.com>
2023-09-13 20:20:12 -0700

    refactor the way we send template email

--- lib/cursif_web/emails/user_email.ex
+++ lib/cursif_web/emails/user_email.ex
@@ -6,13 +6,13 @@ defmodule CursifWeb.Emails.UserEmail do
   import Swoosh.Email
   alias Cursif.Mailer
 
-  defp send_email(user, subject, base_url) do
+  defp send_email(user, subject, base_url, context) do
     email =
       new()
       |> from({"Cursif", "cursif@codesociety.xyz"})
       |> to(user.email)
       |> subject(subject)
-      |> render_body("welcome.html", %{username: user.username, base_url: base_url})
+      |> render_body(context, %{username: user.username, base_url: base_url})
     
     with {:ok, _metadata} <- Mailer.deliver(email) do
       {:ok, email}
@@ -25,6 +25,15 @@ defmodule CursifWeb.Emails.UserEmail do
   def send_confirmation_email(user, token) do
     client_url = Application.get_env(:cursif, :client_url)
     base_url = "#{client_url}/confirm?token=#{token}"
-    send_email(user, "Welcome to Cursif ~ Email Verification", base_url)
+    send_email(user, "Welcome to Cursif ~ Email Verification", base_url, "welcome.html")
+  end
+
+  @doc """
+  Sends a password reset email to a user.
+  """
+  def send_password_reset_email(user, token) do
+    client_url = Application.get_env(:cursif, :client_url)
+    base_url = "#{client_url}/reset-password?token=#{token}"
+    send_email(user, "Cursif ~ Password Reset", base_url, "reset.html")
   end
 end
