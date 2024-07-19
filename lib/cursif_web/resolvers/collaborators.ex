2756b31509b3c437632b97c57af503c903dec424
Chris <rolletchris1@gmail.com>
2024-07-18 19:16:15 -0700

    Added email to user id mapping for adding collaborator

--- lib/cursif_web/resolvers/collaborators.ex
+++ lib/cursif_web/resolvers/collaborators.ex
@@ -1,6 +1,7 @@
 defmodule CursifWeb.Resolvers.Collaborators do
 	alias Cursif.Notebooks
 	alias Cursif.Notebooks.Collaborator
+	alias Cursif.Accounts
 
 	@spec add_collaborator(map(), map()) :: {:ok, Collaborator.t()}  | {:error, atom()}
 	def add_collaborator(collaborator, %{context: %{current_user: current_user}}) do
@@ -8,8 +9,11 @@ defmodule CursifWeb.Resolvers.Collaborators do
 			collaborator.notebook_id,
 			owner: current_user
 		)
-			
-		case Notebooks.add_collaborator(collaborator) do
+
+		user = Accounts.get_user_by_email!(collaborator.email)
+		collaborator_attrs = %{notebook_id: collaborator.notebook_id, user_id: user.id}
+		
+		case Notebooks.add_collaborator(collaborator_attrs) do
 			{:ok, collaborator} -> {:ok, collaborator}
 			{:error, changeset} -> {:error, changeset}
 		end
