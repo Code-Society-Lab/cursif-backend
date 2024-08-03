864e795d4fa2457644bf0cc966d81c61b516a968
Chris <rolletchris1@gmail.com>
2024-08-03 12:41:58 -0700

    fix test

--- test/cursif_web/schema/account_types_test.exs
+++ test/cursif_web/schema/account_types_test.exs
@@ -91,7 +91,6 @@ defmodule CursifWeb.Schema.AccountTypesTest do
         "query" => """
           mutation Register($email: String!, $username: String!, $password: String!) {
             register(email: $email, username: $username, password: $password) {
-              id
               email
               username
             }
